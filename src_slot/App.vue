b8c1ccc5c27be368f9881875d681a03b8f47951f
justin <464241614@qq.com>
2020-05-30 23:55:44 +0800

    slot

---
+++ src_slot/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Child>
+      <template>hello</template>
+      <template slot="shang">
+        <h2>德云社</h2>
+        <p>孟鹤堂</p>
+        <p>周久良</p>
+      </template>
+      <span slot-scope="hello" v-show="isShow" slot="list">
+        √{{hello.person.name}}
+      </span>
+    </Child>
+    <Child>
+      <template slot="xia">
+        <h2>歌星</h2>
+        <p>邓紫棋</p>
+        <p>毛不易</p>
+      </template>
+    </Child>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Child from "./components/Child";
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return {
+      isShow: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src_slot/components/Child.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>固定不变的</h1>
+    <slot></slot>
+
+    <slot name="shang"></slot>
+    <slot name="xia"></slot>
+    <ul>
+      <li v-for="person in persons" :key="person.id">
+        <slot name="list" :person='person'>
+          {{person.name}}
+        </slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+      persons: [
+        { id: 1, name: "秦霄贤" },
+        { id: 2, name: "孙九香" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
